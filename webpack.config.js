8422c13efea8a5a0e4a9cc4462bc2eeacf6a4404
TheLazyDev <bhavarshs@gmail.com>
2017-10-30 16:51:45 +0530

    Fixing prod building issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
-const env = require('dotenv');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if(process.env.NODE_ENV === 'test'){
-   env.config({path:'.env.test'})
+   require('dotenv').config({path:'.env.test'})
 } else if(process.env.NODE_ENV === 'development'){
-    env.config({path:'.env.development'})
+    require('dotenv').config({path:'.env.development'})
 }
 
 
